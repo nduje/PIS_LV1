008f78fca29747b40e05b0d1e78205b8e2375929
nduje <dujerancic@gmail.com>
2022-03-16 17:31:50 +0100

    Dodana promjena u kodu

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace PIS_Vjezba1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Novo");
         }
     }
 }
